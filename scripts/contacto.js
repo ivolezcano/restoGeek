a160868cb12244778a313af7ed355fb3e89721cc
Arturo Gonzales Rodriguez <adgr_10@hotmail.com>
2023-10-15 02:08:34 -0300

    5_commit_page_contacto

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -1,38 +1,59 @@
 document.addEventListener("DOMContentLoaded", function () {
   var formulario = document.getElementById("miFormulario");
+  var validarReCaptcha = document.getElementById("validarReCaptcha");
+  var botonEnviar = document.getElementById("btnSendF");
 
   formulario.addEventListener("submit", function (event) {
-    // Obtiene la respuesta del reCAPTCHA
+    event.preventDefault();
+
     var response = grecaptcha.getResponse();
 
-    // Si la respuesta está vacía, evita que el formulario se envíe
     if (response.length === 0) {
-      event.preventDefault();
-      document.getElementById("validarReCaptcha").innerHTML =
+      validarReCaptcha.innerHTML =
         "Por favor, completa el reCAPTCHA antes de enviar el formulario.";
     } else {
-      event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-
-      var formulario = document.getElementById("miFormulario");
-
       fetch("https://formkeep.com/f/93382513db8e", {
         method: "POST",
         body: new FormData(formulario),
       })
         .then((response) => {
           if (response.ok) {
-            // El formulario se envió correctamente
+            botonEnviar.textContent = "¡Formulario Enviado!";
             console.log("Formulario enviado correctamente");
-            // Puedes hacer algo después de que el formulario se envíe, si es necesario
             formulario.reset();
-          } else {
-            // El formulario no se pudo enviar correctamente
-            console.error("Error al enviar el formulario");
+            grecaptcha.reset();
           }
         })
         .catch((error) => {
           console.error("Error al enviar el formulario: ", error);
         });
     }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var nombreInput = document.getElementById("nombre");
+  var apellidosInput = document.getElementById("apellidos");
+  var emailInput = document.getElementById("email");
+  var telefonoInput = document.getElementById("telefono");
+
+  nombreInput.addEventListener("input", function () {
+    var nombreLabel = document.querySelector("label[for='nombre']");
+    nombreLabel.textContent = "Nombre/s:";
+  });
+
+  apellidosInput.addEventListener("input", function () {
+    var apellidosLabel = document.querySelector("label[for='apellidos']");
+    apellidosLabel.textContent = "Apellido/s:";
+  });
+
+  emailInput.addEventListener("input", function () {
+    var emailLabel = document.querySelector("label[for='email']");
+    emailLabel.textContent = "Email:";
+  });
+
+  telefonoInput.addEventListener("input", function () {
+    var telefonoLabel = document.querySelector("label[for='telefono']");
+    telefonoLabel.textContent = "Teléfono:";
   });
 });
