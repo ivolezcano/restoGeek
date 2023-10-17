7fc6ca5bf5dd48476a51aee4a6e14e4b013afd79
Ivan Lezcano <lezcamati@gmail.com>
2023-10-16 21:00:46 -0300

    Navbar vinculacion

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Redes footer
 const menu=document.querySelector(".menu");
 const toggle=document.querySelector(".toggle");
 toggle.addEventListener("click",()=>{
   menu.classList.toggle("active");
 })
+// Menu hamburguesa
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.querySelector(".navLinks");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
