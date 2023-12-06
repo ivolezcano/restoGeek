a8002c99eeb061fc64edb0520f5210bd1c588e3b
Ivan Lezcano <lezcamati@gmail.com>
2023-12-06 19:21:08 -0300

    footercomponent 30%

--- Sprint2/frontend/restogeek/src/App.vue
+++ Sprint2/frontend/restogeek/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FooterComponent />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FooterComponent from '@/components/footer/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, 
+    FooterComponent,
   }
 }
 </script>
